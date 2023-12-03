2386768173c81cc567b9423372b4ede2f5fa4ba1
kobakoba5884 <kobanminmin58@gmail.com>
2023-12-03 22:37:17 +0900

    add search function

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
-import { Post, useGetPostsQuery } from "./__generated__/graphql";
+import { useState } from "react";
+import {
+  Post,
+  useGetPostsLazyQuery,
+  useGetPostsQuery,
+} from "./__generated__/graphql";
 import { InputField } from "./components/Elements/InputField";
 import { Layout } from "./components/Layout";
 import { PostListItem } from "./components/PostListItem";
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
   const { data, loading, error } = useGetPostsQuery();
 
-  console.log(data?.posts)
+  const filteredPosts = searchTerm
+    ? data?.posts?.filter((post) =>
+        post?.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        post?.excerpt.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        post?.tags.some(tag => tag?.name.toLowerCase().includes(searchTerm.toLowerCase()))
+      )
+    : data?.posts;
+
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value.trim();
+    setSearchTerm(value);
+  };
 
   return (
     <>
@@ -14,11 +31,14 @@ function App() {
         <div className="max-w-2xl mx-auto p-5 mb-3">
           <h2 className="mb-5 font-bold text-4xl">All Post</h2>
           <div>
-            <InputField placeholder="search" />
+            <InputField placeholder="search" value={searchTerm} onChange={handleSearch} />
           </div>
         </div>
-        {data?.posts.map((postItem) => (
-          <PostListItem key={postItem.id} postItem={postItem as Partial<Post>} />
+        {filteredPosts?.map((postItem) => (
+          <PostListItem
+            key={postItem.id}
+            postItem={postItem as Partial<Post>}
+          />
         ))}
       </Layout>
     </>
